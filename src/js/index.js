9474924cceed7490623a133f624e6d2b9c8c0f95
RudychikFedorV <rudychik.fedor@gmail.com>
2024-02-17 09:05:15 +0200

    How we work

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,6 @@
 // import header from './modules/header.js';
 // header();
+<script>
+  document.querySelector('.icon').addEventListener('click', function(){" "}
+  {this.classList.toggle("icon--active")});
+</script>;
