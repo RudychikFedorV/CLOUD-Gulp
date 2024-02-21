885f37e0a8cc70c4970adc86d9b2746e4d3b0250
RudychikFedorV <rudychik.fedor@gmail.com>
2024-02-21 20:26:58 +0200

    ourExperience btn mixins

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,2 @@
 // import header from './modules/header.js';
 // header();
-<script>
-  document.querySelector('.icon').addEventListener('click', function(){" "}
-  {this.classList.toggle("icon--active")});
-</script>;
