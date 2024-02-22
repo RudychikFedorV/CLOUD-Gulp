2825d66be24295052072d90c30f9166511e34126
RudychikFedorV <rudychik.fedor@gmail.com>
2024-02-22 22:05:31 +0200

    form

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,10 @@
 // import header from './modules/header.js';
 // header();
 
+document.querySelector(".icon").addEventListener("click", function () {
+  this.classList.toggle("icon--active");
+});
+
 // import Swiper bundle with all modules installed
 import Swiper from "swiper/bundle";
 
@@ -9,7 +13,7 @@ import "swiper/css/bundle";
 
 const swiper = new Swiper(".swiper", {
   // Optional parameters
-//   direction: "vertical",
+  //   direction: "vertical",
   loop: true,
   slidesPerView: 2.3,
 
@@ -28,4 +32,20 @@ const swiper = new Swiper(".swiper", {
   scrollbar: {
     el: ".swiper-scrollbar",
   },
+});
+// JavaScript
+var mySwiper = new Swiper('.swiper-container', {
+  // настройки Swiper...
+});
+
+// Добавьте обработчик изменения размера окна
+window.addEventListener('resize', function () {
+  if (window.innerWidth <= 768) {
+    mySwiper.params.slidesPerView = 1; // Настройте количество видимых слайдов для маленьких экранов
+  } else {
+    mySwiper.params.slidesPerView = 3; // Возвращаем к количеству слайдов для больших экранов
+  }
+  
+  // Переинициализируем Swiper
+  mySwiper.update();
 });
